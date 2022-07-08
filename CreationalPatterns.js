cf37388a3d06e8281613f9a14483384d5bc673ad
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-09 01:46:48 +0300

    Singleton pattern

--- CreationalPatterns.js
+++ CreationalPatterns.js
@@ -1,21 +1,29 @@
+let instance = null;
+
 // Class design pattern
 class Car {
   constructor(doors, engine, color) {
-    this.doors = doors;
-    this.engine = engine;
-    this.color = color;
+    // Singleton pattern
+    if (!instance) {
+      this.doors = doors;
+      this.engine = engine;
+      this.color = color;
+      instance = this;
+    } else {
+      return instance;
+    }
   }
 }
 
 const civic = new Car(4, "V6", "grey");
+const honda = new Car(4, "V8", "red");
 
 // Constructor pattern
-
 class SUV extends Car {
-    constructor(doors, engine, color) {
-        super(doors, engine, color);
-        this.wheels = 4; 
-    }
+  constructor(doors, engine, color) {
+    super(doors, engine, color);
+    this.wheels = 4;
+  }
 }
 
 const cx5 = new SUV(4, "V8", "red");
