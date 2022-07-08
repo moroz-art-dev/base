c0bb9b20a44f0826e9ea89b775942e32a9a71131
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-09 02:47:24 +0300

    Abstract factory pattern

--- CreationalPatterns.js
+++ CreationalPatterns.js
@@ -8,15 +8,15 @@ class Car {
 }
 
 // Constructor pattern
-class SUV extends Car {
-  constructor(doors, engine, color) {
-    super(doors, engine, color);
-    this.wheels = 4;
-  }
-}
+// class SUV extends Car {
+//   constructor(doors, engine, color) {
+//     super(doors, engine, color);
+//     this.wheels = 4;
+//   }
+// }
 
 // Factory pattern
-class carFactory {
+class CarFactory {
   createCar(type) {
     switch (type) {
       case "civic":
@@ -27,5 +27,36 @@ class carFactory {
   }
 }
 
-const factory = new carFactory();
-const myHonda = factory.createCar("honda")
+// Abstract factory pattern
+class SUV {
+  constructor(doors, engine, color) {
+    this.doors = doors;
+    this.engine = engine;
+    this.color = color;
+  }
+}
+
+class SuvFactory {
+  createCar(type) {
+    switch (type) {
+      case "sx5":
+        return new Car(4, "V6", "grey");
+      case "sante fe":
+        return new Car(2, "V8", "red");
+    }
+  }
+}
+
+const carFactory = new CarFactory();
+const suvFactory = new SuvFactory();
+
+const autoManufacturer = (type, model) => {
+  switch (type) {
+    case "car":
+      return carFactory.createCar(model);
+    case "suv":
+      return suvFactory.createCar(model);
+  }
+};
+
+const cx5 = autoManufacturer("suv", "sx5");
