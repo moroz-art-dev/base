d6f9cf12804613f6f1d8ff8106441711b623d731
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-15 01:58:35 +0300

    Behavioral Patterns: Iterator pattern

--- BehavioralPatterns.js
+++ BehavioralPatterns.js
@@ -1,65 +1,90 @@
 // Observer pattern
 class Car {
-    constructor(gas) {
-        this.gas = gas;
-    }
+  constructor(gas) {
+    this.gas = gas;
+  }
 
-    setGasLevel(val) {
-        this.gas = val;
-        this.notifyAll();
-    }
+  setGasLevel(val) {
+    this.gas = val;
+    this.notifyAll();
+  }
 
-    register(observer) {
-        this.actions.push(observer);
-    }
+  register(observer) {
+    this.actions.push(observer);
+  }
 
-    unregister(observer) {
-        this.actions.remove.filter(function(el) {
-            return el !== observer;
-        });
-    }
+  unregister(observer) {
+    this.actions.remove.filter(function (el) {
+      return el !== observer;
+    });
+  }
 
-    notifyAll() {
-        return this.actions.forEach(function(el) {
-            el.update(this);
-        }.bind(this));
-    }
+  notifyAll() {
+    return this.actions.forEach(
+      function (el) {
+        el.update(this);
+      }.bind(this)
+    );
+  }
 }
 
 class consumption {
-    update(car) {
-        car.gas = car.gas + 1;
-    }
+  update(car) {
+    car.gas = car.gas + 1;
+  }
 }
 
 // State pattern
 // React, Angular
 class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        news1: {
-          type: 'top-headlines',
-          query: 'sources=bbc-news'
-        },
-        news2: {
-          type: 'everything',
-          query: 'domains=techcrunch.com&language=en'
-        },
-        news3: {
-          type: 'everything',
-          query: 'domains=comicbookmovie.com&language=en'
-        }
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      news1: {
+        type: "top-headlines",
+        query: "sources=bbc-news",
+      },
+      news2: {
+        type: "everything",
+        query: "domains=techcrunch.com&language=en",
+      },
+      news3: {
+        type: "everything",
+        query: "domains=comicbookmovie.com&language=en",
+      },
+    };
+  }
 }
 
 class News extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        news: [],
-        error: false,
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+      error: false,
+    };
+  }
+}
+
+// Chain of responsibility
+
+// Iterator pattern
+
+newsfeeds = [
+  {
+    type: "top-headlines",
+    query: "sources=bbc-news",
+  },
+  {
+    type: "everything",
+    query: "domains=techcrunch.com&language=en",
+  },
+  {
+    type: "everything",
+    query: "domains=comicbookmovie.com&language=en",
+  },
+];
+
+for (let feed of newsfeeds) {
+    console.log(feed.type);
 }
